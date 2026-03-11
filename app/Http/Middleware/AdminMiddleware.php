b124bfb0c071fac42f0e230b08b1edc5260d421e
unknown <kiransimlot@gmail.com>
2026-03-11 23:39:20 +0530

    11-01-2026 git push file

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->check() || auth()->user()->is_admin != 1) {
+            abort(403);
+        }
+        return $next($request);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('front');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::middleware(['auth','admin'])->group(function () {
+
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    });
+
+});
